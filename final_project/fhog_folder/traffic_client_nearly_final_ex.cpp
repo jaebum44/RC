c2c85cb47ea4f718714945c768a15d023e481f77
yeogue <yeogue@github.com>
2017-09-18 14:39:56 +0900

    update

--- final_project/fhog_folder/traffic_client_nearly_final_ex.cpp
+++ final_project/fhog_folder/traffic_client_nearly_final_ex.cpp
@@ -45,8 +45,9 @@ namespace HSV_CONST
 	};
 }
 
-void*gstream_recv(void*);
-void*img_handler(void*);
+void* img_recv(void*);
+void* img_handler_tl(void*);
+void* img_handler_ts(void*);
 int tlight_msg_handler(Mat &);
 int tsign_msg_handler(Mat &, int, dlib::rectangle &, char*);
 void create_msg_box(std::vector<dlib::rectangle> &, dlib::rectangle &);
@@ -60,7 +61,9 @@ float dist_detect(dlib::rectangle &);
 
 
 Mat img;
-sem_t recv_sync, show_sync;
+sem_t recv_sync;
+sem_t show_sync;
+pthread_mutex_t I_Mutex;
 int red_sign_on, child_sign_on, buf;
 
 int main(int argc, char** argv)
@@ -70,12 +73,11 @@ int main(int argc, char** argv)
 	struct sockaddr_in server_addr;
 	char*serverAddr;
 	int serverPort; 
-	int thr_id, status;
+	int thr_id[2], status;
 	pthread_t pid[2];
 	int len;
 	
-	img = Mat::zeros(240, 320,CV_8UC3);
-
+	img = Mat::zeros(240, 320, CV_8UC3);
 	
 	if(argc < 3)
 	{
@@ -85,7 +87,6 @@ int main(int argc, char** argv)
 
 	serverAddr = argv[1];
 	serverPort = atoi(argv[2]);
-
 
 	if((connSock=socket(PF_INET, SOCK_STREAM, 0)) < 0) 
 	{
@@ -106,8 +107,16 @@ int main(int argc, char** argv)
 
 	printf("Traffic Client connect to Traffic server \n");
 
-	thr_id=pthread_create(&pid[0], NULL,gstream_recv,(void*)&connSock);
-	thr_id=pthread_create(&pid[1], NULL,img_handler,(void*)&connSock);
+	if(thr_id[0]=pthread_create(&pid[0], NULL,img_recv,NULL) < 0)
+	{
+		perror("pthread1 failed");
+		return -1;
+	}
+	if(thr_id[1]=pthread_create(&pid[1], NULL,img_handler_ts,(void*)&connSock) < 0)
+	{
+		perror("pthread2 failed");
+		return -1;
+	}
 
 	pthread_join(pid[0], (void**)&status);
 	pthread_join(pid[1], (void**)&status);
@@ -117,12 +126,12 @@ int main(int argc, char** argv)
 	return 0;
 }
 
-void*gstream_recv(void*arg)
+void* img_recv(void*arg)
 {
 
 	VideoCapture vcap; 
  
-	string videoStreamAddress = "rtsp://192.168.1.33:8554/test";	
+	string videoStreamAddress = "rtsp://192.168.1.105:8554/test";	
 	
 	vcap.open(videoStreamAddress);
 
@@ -134,47 +143,107 @@ void*gstream_recv(void*arg)
 	} 		
 }
 
-void*img_handler(void*arg)
+void* img_handler_ts(void*arg)
 {
-
 	Mat img_show;
 
-	int connSock=*(int*)arg;
+	int connSock = *(int*)arg;
 	int detect_color = 0;
+	int status;
+	pthread_t pid;
 	
-	typedef dlib::scan_fhog_pyramid<dlib::pyramid_down<6> > image_scanner_type; 
+	typedef dlib::scan_fhog_pyramid<dlib::pyramid_down<6>> image_scanner_type; 
 	
-	image_scanner_type scanner_tl;
 	image_scanner_type scanner_ts;
 
-	dlib::object_detector<image_scanner_type> detector_tlight;
 	dlib::object_detector<image_scanner_type> detector_tsign;
 	dlib::matrix <dlib::bgr_pixel>cimg;
 		
-	vector<dlib::rectangle> dets_tlight;
 	vector<dlib::rectangle> dets_tsign;
 
-	dlib::deserialize("tlight3_detector.svm") >> detector_tlight;
 	dlib::deserialize("tsign_detector.svm") >> detector_tsign;
+
+	scanner_ts.set_detection_window_size(80, 80); 
+	scanner_ts.set_max_pyramid_levels(1); 
+
+	if(pthread_create(&pid, NULL,img_handler_tl,(void*)&connSock) < 0)
+	{
+		perror("img_hander_tl failed");
+	}
 
 	while(1)
 	{
-	
 		sem_wait(&recv_sync);
 
+		pthread_mutex_lock(&I_Mutex);
 		dlib::assign_image(cimg,dlib::cv_image<dlib::rgb_pixel>(img));
+		pthread_mutex_unlock(&I_Mutex);
+	
 		dlib::pyramid_up(cimg);	
 
-		scanner_tl.set_detection_window_size(80, 170); 
-		scanner_ts.set_detection_window_size(80, 80); 
-		scanner_tl.set_max_pyramid_levels(1);	
-		scanner_ts.set_max_pyramid_levels(1); 
+		dets_tsign = detector_tsign(cimg);
+
+		if(dets_tsign.size())
+		{
+			cout<<"deteced child == slow"<<endl;
+			int ret = system("mpg123 child_sign_voice.mp3");
+			child_sign_on = 1;
+		}
+		else
+			cout<<"no detected child == fast"<<endl;
+
+		buf= red_sign_on | child_sign_on; // 0 fast, 1 slow, 2 stop, 3 slow and stop
 		
+		if(send(connSock, &buf, sizeof(buf), 0) < 0) 
+		{
+			perror("send to traffic server failed");
+		}
+
+		child_sign_on = 0;
+			
+		cvtColor(img, img_show, CV_RGB2BGR);
+		//pyrUp(img_show, img_show, Size(img.cols*2, img.rows*2));
+      	imshow("Output Window", img_show); 
+		waitKey(1);
+	}
+
+	pthread_join(pid, (void**)&status);
+}
+
+
+void* img_handler_tl(void*arg)
+{
+	int connSock = *(int*)arg;
+	int detect_color = 0;
+	
+	typedef dlib::scan_fhog_pyramid<dlib::pyramid_down<6>> image_scanner_type; 
+	
+	image_scanner_type scanner_tl;
+
+	dlib::object_detector<image_scanner_type> detector_tlight;
+	dlib::matrix <dlib::bgr_pixel>cimg;
+		
+	vector<dlib::rectangle> dets_tlight;
+
+	dlib::deserialize("tlight3_detector.svm") >> detector_tlight;
+
+	scanner_tl.set_detection_window_size(80, 170); 
+	scanner_tl.set_max_pyramid_levels(1);	
+
+	while(1)
+	{
+		sem_wait(&recv_sync);
+
+		pthread_mutex_lock(&I_Mutex);
+		dlib::assign_image(cimg,dlib::cv_image<dlib::rgb_pixel>(img));
+		pthread_mutex_unlock(&I_Mutex);
+
+		dlib::pyramid_up(cimg);	
+
 		detect_color = tlight_msg_handler(img);
 
 		if(detect_color)
 		{	
-
 			dets_tlight = detector_tlight(cimg);
 	
 			if(dets_tlight.size())
@@ -182,54 +251,30 @@ void*img_handler(void*arg)
 				cout<<"detected traffic light RED"<<endl;
 				red_sign_on  = 2;
 			}
+			else
+				red_sign_on = 0;
 
 			detect_color = 0;
 		}
-
-		dets_tsign = detector_tsign(cimg);
-
-		if(dets_tsign.size())
-		{
-			cout<<"deteced child"<<endl;
-			int ret = system("mpg123 child_sign_voice.mp3");
-			child_sign_on = 1;
-		}
-
-		buf= red_sign_on | child_sign_on; // 0 fast, 1 slow, 2 stop, 3 slow and stop
-
-
-		if(send(connSock, &buf, sizeof(buf), 0) < 0) 
-		{
-			perror("send to traffic server failed");
-		}
-
-		red_sign_on = 0;
-		child_sign_on = 0;
-			
-		cvtColor(img, img_show, CV_RGB2BGR);
-		pyrUp(img_show, img_show, Size(img.cols*2, img.rows*2));
-      	imshow("Output Window", img_show); 
-		waitKey(1);
-	}
-
-}
-
-void create_msg_box(std::vector<dlib::rectangle> &dets, dlib::rectangle &r)
-{
-	if(dets.size())
-	{
-		if(dets[0].left() > 0)
-			r.left() = dets[0].left();
-		else
-			r.left() = 0;
-		r.bottom() = dets[0].bottom();
-		r.right() = dets[0].right();
-		if(dets[0].top() > 0)
-			r.top() = dets[0].top();
-		else
-			r.top() = 0;
 	}
 }
+
+//void create_msg_box(std::vector<dlib::rectangle> &dets, dlib::rectangle &r)
+//{
+//	if(dets.size())
+//	{
+//		if(dets[0].left() > 0)
+//			r.left() = dets[0].left();
+//		else
+//			r.left() = 0;
+//		r.bottom() = dets[0].bottom();
+//		r.right() = dets[0].right();
+//		if(dets[0].top() > 0)
+//			r.top() = dets[0].top();
+//		else
+//			r.top() = 0;
+//	}
+//}
 
 int tlight_msg_handler(Mat &img)
 {
@@ -257,30 +302,30 @@ int tlight_msg_handler(Mat &img)
 	return 0;
 }
 
-int tsign_msg_handler(Mat &img, int dets, dlib::rectangle &r, char*distance_msg)
-{
-	
-	float distance;
-	int i = 0;
-	int red_positive;
-		
-	if(dets)
-	{
-		distance=dist_detect(r);
-		sprintf(distance_msg,"  DISTANCE : %.2fCM\n",distance);
-		
-		if(distance < 20)
-		{
-			child_sign_on = 1;
-			cout<<"Stop"<<' '<<i<<endl;
-			i++;
-		}
-	}
-
-	sprintf(distance_msg,"  detected sign : %d\n",dets);
-
-	return 0;
-}
+//int tsign_msg_handler(Mat &img, int dets, dlib::rectangle &r, char*distance_msg)
+//{
+//	
+//	float distance;
+//	int i = 0;
+//	int red_positive;
+//		
+//	if(dets)
+//	{
+//		distance=dist_detect(r);
+//		sprintf(distance_msg,"  DISTANCE : %.2fCM\n",distance);
+//		
+//		if(distance < 20)
+//		{
+//			child_sign_on = 1;
+//			cout<<"Stop"<<' '<<i<<endl;
+//			i++;
+//		}
+//	}
+//
+//	sprintf(distance_msg,"  detected sign : %d\n",dets);
+//
+//	return 0;
+//}
 
 float dist_detect(dlib::rectangle &r)
 {
@@ -294,7 +339,6 @@ float dist_detect(dlib::rectangle &r)
 
 int hsv_handler(Mat &img)
 {
-
 	Mat img_for_detect[2];
 	int red_positive, green_positive;
 
